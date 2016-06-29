ef13e97ae9c7aa8905a309b34bbdd1500e896716
François Leurent <131.php@cloudyks.org>
2016-06-29 15:41:46 +0200

    Export window.requestAnimationFrame pollyfill

---
+++ window/requestAnimationFrame.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
