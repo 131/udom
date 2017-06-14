164a0b897a09566dce6b0f18714044990915afb9
Kolosowski florent <fkolosowski@ivsweb.com>
2017-06-14 17:40:08 +0200

    bump toQueryHash

--- element/toQueryHash.js
+++ element/toQueryHash.js
@@ -1,23 +1,43 @@
 "use strict";
 
-var forEach = require('mout/array/forEach');
+const forEach = require('mout/array/forEach');
 
 module.exports = function(dom) {
-  var queryString = {}, value;
+  var queryString = {};
+  var value;
 
-  forEach(dom.querySelectorAll("input"), function(el) {
+  forEach(dom.querySelectorAll('input'), function(el) {
+    var type = el.type;
+    var date_mode = (el.type == 'date' || el['value-type'] == 'date');
 
-    var type = el.type,
-        date_mode = (el.type == 'date' || el['value-type'] == 'date');
-
-    if (!el.name || el.disabled || type == 'submit' || type == 'reset' || type == 'file' || type == 'image')
+    if (!el.name || el.disabled || type == 'submit' || type == 'reset' || type == 'file' || type == 'image') {
       return;
+    }
 
     value = el.value;
-    if(date_mode && value) value = Date.parse(value).format('%s');
-    if(date_mode && !value) value = null;
-    queryString[el.name] = value;
+    if (date_mode && value) {
+      value = Date.parse(value).format('%s');
+    }
+    if (date_mode && !value) {
+      value = null;
+    }
 
+    if (type == 'radio' && el.checked) {
+      queryString[el.name] = value;
+    } else if (type == 'checkbox') {
+      if (!queryString[el.name]) {
+        queryString[el.name] = value;
+      } else {
+        if (typeof queryString[el.name] == 'string') {
+          let temp = queryString[el.name];
+          queryString[el.name] = [];
+          queryString[el.name].push(temp);
+        }
+        queryString[el.name].push(value);
+      }
+    } else if (type != 'radio' && type != 'checkbox') {
+      queryString[el.name] = value;
+    }
   });
   return queryString;
 };
