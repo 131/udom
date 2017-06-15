9853b6374613e68e0cb9250dd37f356a056ef2dc
Kolosowski florent <fkolosowski@ivsweb.com>
2017-06-15 12:23:41 +0200

    bump isVisible

--- element/isVisible.js
+++ element/isVisible.js
@@ -1,6 +1,6 @@
 "use strict";
 
-
+/*
 function isElementInViewport (el) {
 
     var rect = el.getBoundingClientRect();
@@ -11,19 +11,23 @@ function isElementInViewport (el) {
       && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
       && rect.left <= (window.innerWidth || document.documentElement.clientWidth)
     );
-}
+}*/
 
 
 module.exports = function(element){
-    if (element.offsetWidth === 0 || element.offsetHeight === 0) return false;
+  if (element.offsetWidth === 0 || element.offsetHeight === 0) return false;
 
-   /* var height = document.documentElement.clientHeight,
-        on_top = function(r) {
-          var x = (r.left + r.right)/2, y = Math.max(0, (r.top + r.bottom)/2);
-          return element.contains( document.elementFromPoint(x, y) );
-        };
-    */
+  var height = root.documentElement.clientHeight;
+  var rects = element.getClientRects();
+  var on_top = function(r) {
+    var x = (r.left + r.right)/2, y = (r.top + r.bottom)/2;
+    return element.contains( root.elementFromPoint(x, y) );
+  };
 
-    var in_viewport = isElementInViewport(element);
-    return in_viewport;
+  for (var i = 0, l = rects.length; i < l; i++) {
+    var r = rects[i];
+    var in_viewport = r.top > 0 ? r.top <= height : (r.bottom > 0 && r.bottom <= height);
+    if (in_viewport && on_top(r)) return true;
+  }
+  return false;
 };
